6464fb80ef9783ba9405e99984c8a1c0364fdabf
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-06-22 12:04:35 -0500

    edit the data type for the MathController to int after walkthrough

--- src/main/java/com/codeup/blog/controllers/MathController.java
+++ src/main/java/com/codeup/blog/controllers/MathController.java
@@ -10,26 +10,26 @@ public class MathController {
 
     @GetMapping("/add/{num1}/and/{num2}")
     @ResponseBody
-    public String add(@PathVariable int num1, @PathVariable int num2) {
-        return "result of " + num1 + " +  " +num2 + " = " + (num1 + num2);
+    public int add(@PathVariable int num1, @PathVariable int num2) {
+        return (num1 + num2);
     }
 
     @GetMapping("/subtract/{num1}/from/{num2}")
     @ResponseBody
-    public String subtract(@PathVariable int num1, @PathVariable int num2) {
-        return "result of " + num2 + " - " + num1 + " = " + (num2 - num1);
+    public int subtract(@PathVariable int num1, @PathVariable int num2) {
+        return (num2 - num1);
     }
 
     @GetMapping("/multiply/{num1}/and/{num2}")
     @ResponseBody
-    public String multiply(@PathVariable int num1, @PathVariable int num2) {
-        return "result of " + num1 + " x " + num2 + " = " + (num1 * num2);
+    public int multiply(@PathVariable int num1, @PathVariable int num2) {
+        return (num1 * num2);
     }
 
     @GetMapping("/divide/{num1}/by/{num2}")
     @ResponseBody
-    public String divide(@PathVariable int num1, @PathVariable int num2) {
-        return "result of " + num1 + " / " + num2 + " = " + (num1 / num2);
+    public int divide(@PathVariable int num1, @PathVariable int num2) {
+        return (num1 / num2);
     }
 }
 
