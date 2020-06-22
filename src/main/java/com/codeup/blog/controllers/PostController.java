2d191d8dacdb201e5cd55aaac4389af5fbbf23c2
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-06-22 11:38:54 -0500

    added ResponseBody annotation

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -4,26 +4,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
+    @ResponseBody
     public String postsIndex() {
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
+    @ResponseBody
     public String postsView(@PathVariable long id) {
         return "posts/show";
     }
 
     @GetMapping("/posts/create")
+    @ResponseBody
     public String viewPostsCreate() {
         return "posts/create";
     }
 
     @PostMapping("/posts/create")
+    @ResponseBody
     public String postsCreate() {
         return "posts/create";
     }
