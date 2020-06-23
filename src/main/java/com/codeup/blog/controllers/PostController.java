c28e876b264bcc764543f966e43fe005593c7138
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-06-23 00:18:06 -0500

    TODO: finish index page

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -1,18 +1,27 @@
 package com.codeup.blog.controllers;
 
+import com.codeup.blog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String index() {
-        return "posts index page";
+//    @ResponseBody
+    public String index(Model model) {
+//        return "posts index page";
+        ArrayList<Post> posts = new ArrayList<>();
+        posts.add(new Post("First post", "Hello! Today is Monday."));
+        posts.add(new Post("Second post", "Tomorrow is Tuesday and it's going to rain."));
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
