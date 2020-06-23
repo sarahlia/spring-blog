7fbace8d64fc8df3e7f9b324f012ccea12614e86
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-06-23 11:32:23 -0500

    added the Ad model, controller, view from the lecture (OPT)

---
+++ src/main/java/com/codeup/blog/controllers/AdsController.java
@@ -0,0 +1,44 @@
+package com.codeup.blog.controllers;
+
+import com.codeup.blog.models.Ad;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+
+public class AdsController {
+
+    @GetMapping("/ads")
+//    @RequestMapping(value = "/ads", method = RequestMethod.GET)
+    public String index(Model model){
+        ArrayList<Ad> adsList = new ArrayList<>();
+        adsList.add(new Ad("PS1", "Used"));
+        adsList.add(new Ad("PS2", "Used"));
+        adsList.add(new Ad("PS4", "Used"));
+        adsList.add(new Ad("SNES", "Used"));
+        model.addAttribute("noAdsFound", adsList.size() == 0);
+        model.addAttribute("ads", adsList);
+        return "ads/index";
+    }
+
+    @GetMapping("/ads/{id}")
+    @ResponseBody
+    public String show(@PathVariable long id){
+        return "view an individual ad with the id of: " + id;
+    }
+
+    @GetMapping("/ads/create")
+    @ResponseBody
+    public String showForm(){
+        return "view the form for creating an ad";
+    }
+
+    @PostMapping("/ads/create")
+    @ResponseBody
+    public String save(){
+        return "create a new ad";
+    }
+}
